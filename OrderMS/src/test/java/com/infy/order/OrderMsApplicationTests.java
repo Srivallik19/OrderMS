e8090ee75e502a8dfd161d54ee36371240927283
jack <srivallikannappan@gmail.com>
2021-06-29 13:26:15 +0530

    Uploaded OrderMS

---
+++ OrderMS/src/test/java/com/infy/order/OrderMsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.infy.order;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class OrderMsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
